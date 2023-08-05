852cfade6310b3cb2edeabed7ae9291b61b22666
vinhhd198c <vinhnqph20805@fpt.edu.vn>
2023-08-05 22:23:21 +0700

    buoi3

---
+++ demoSpring/src/main/java/com/example/demo/buoi3/MayTinh.java
@@ -0,0 +1,42 @@
+package com.example.demo.buoi3;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "may_tinh")
+public class MayTinh {
+
+    @Id
+    @GenericGenerator(name = "gennerator", strategy = "guid", parameters = {})
+    @GeneratedValue(generator = "gennerator")
+    @Column(name = "ma")
+    private String ma;
+
+    @Column(name = "ten")
+    private String ten;
+
+    @Column(name = "gia")
+    private Float gia;
+
+    @Column(name = "bo_nho")
+    private String boNho;
+
+    @Column(name = "mau_sac")
+    private String mauSac;
+
+    @Column(name = "hang")
+    private String hang;
+
+    @Column(name = "mieu_ta")
+    private String mieuTa;
+}
---
+++ demoSpring/src/main/java/com/example/demo/buoi3/MayTinhController.java
@@ -0,0 +1,48 @@
+package com.example.demo.buoi3;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/mayTinh")
+public class MayTinhController {
+    @Autowired
+    private MayTinhService mayTinhService;
+
+    @GetMapping()
+    public ResponseEntity<?> getAll(@RequestParam(defaultValue = "0", value = "page") Integer page) {
+        return ResponseEntity.ok(mayTinhService.getAll(page));
+    }
+
+    @GetMapping("/detail/{ma}")
+    public ResponseEntity<?> getOne(@PathVariable String ma) {
+        return ResponseEntity.ok(mayTinhService.getOne(ma));
+    }
+
+    @PutMapping("/update/{ma}")
+    public ResponseEntity<?> update(@PathVariable String ma, @RequestBody MayTinh mayTinh) {
+        return ResponseEntity.ok(mayTinhService.update(mayTinh,ma));
+    }
+
+    @DeleteMapping("/delete/{ma}")
+    public ResponseEntity<?> delete(@PathVariable String ma) {
+        return ResponseEntity.ok(mayTinhService.delete(ma));
+    }
+
+    @GetMapping("/loc")
+    public ResponseEntity<?> loc() {
+        return ResponseEntity.ok(mayTinhService.loc());
+    }
+
+    @GetMapping("/max")
+    public ResponseEntity<?> max() {
+        return ResponseEntity.ok(mayTinhService.max());
+    }
+
+    @GetMapping("/tong")
+    public ResponseEntity<?> tong() {
+        return ResponseEntity.ok(mayTinhService.tong());
+    }
+}
